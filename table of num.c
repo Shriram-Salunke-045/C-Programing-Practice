c40295842b1bdcb74e576f1c9afa9253e74c9e14
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2023-08-04 17:26:48 +0530

    Update table of num.c

--- table of num.c
+++ table of num.c
@@ -1,17 +1,13 @@
-// Write a program to generate a table of number
+// Write a program to generate a table of number in c language
 
 # include<stdio.h>
-
 int main(){
-	
 	int num,i;
-	
 	printf("Enter the number : ");
 	scanf("%d",&num);
 	
 	for(i=1;i<=10;i++){
 		printf("\n%d * %d = %d\n",num,i,num*i);
 	}
-	
 	return 0;
 }
