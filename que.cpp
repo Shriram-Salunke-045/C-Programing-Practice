5e5bc9bc87df7b04db600215ca10e32b661e0811
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2023-08-04 17:29:41 +0530

    Add files via upload

---
+++ que.cpp
@@ -0,0 +1,11 @@
+ Write a function to find Second Minimum in a Array * Write a function to find Second Maximum in a Array * Write a fucntion to Sort the Array in the given order
+
+
+#include<stdio.h>
+
+find_second_minimum()
+int main(){
+	int array[10]={1,4,5,67,100};
+	int second_minimum = find_second_minimum(array,10);
+	printf("second minimum %d",second_minimum);
+}
