8ed7b825c8338216f43c38d58691754fee8257bc
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2023-08-04 17:25:36 +0530

    Add files via upload

---
+++ table of num.c
@@ -0,0 +1,17 @@
+// Write a program to generate a table of number
+
+# include<stdio.h>
+
+int main(){
+	
+	int num,i;
+	
+	printf("Enter the number : ");
+	scanf("%d",&num);
+	
+	for(i=1;i<=10;i++){
+		printf("\n%d * %d = %d\n",num,i,num*i);
+	}
+	
+	return 0;
+}
